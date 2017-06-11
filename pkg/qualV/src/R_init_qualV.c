7c954b29707dc56ad0f1bbd9bdf934bbc4d38859
petzoldt (Dr ) <thomas.petzoldt@tu-dresden.de>
2017-06-11 19:28:18 +0000

    register lcs as native routine
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/qualv@28 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/qualV/src/R_init_qualV.c
@@ -0,0 +1,30 @@
+#ifndef R_R_H
+# include <R.h>
+#endif
+
+#ifndef R_EXT_DYNLOAD_H_
+# include <R_ext/Rdynload.h>
+#endif
+
+#include <Rinternals.h>
+#include <stdlib.h> // for NULL
+
+extern SEXP  lcs(SEXP, SEXP , SEXP);
+
+static const R_CMethodDef CEntries[] = {
+    {NULL, NULL, 0}
+}; 
+
+static const R_CallMethodDef CallEntries[] = {
+    {"lcs",      (DL_FUNC) &lcs,      3},
+    {NULL,        NULL,               0}
+};
+
+void R_init_deSolve(DllInfo *dll) {
+  // register entry points
+  R_registerRoutines(dll, CEntries, CallEntries, NULL, NULL);
+  
+  // the following two lines protect against accidentially finding entry points
+  R_useDynamicSymbols(dll, FALSE);  // disable dynamic searching
+  //R_forceSymbols(dll, TRUE);      // entry points as R objects, not as strings
+} 
